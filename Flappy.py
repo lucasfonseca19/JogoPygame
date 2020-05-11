4b3f7e27bcd3264416b21f21552241e3882423c3
Lucas <lucasofa808@gmail.com>
2020-05-11 16:47:01 -0300

    Setup inicial da tela
    Importando biblioteca
    Inicializando pygame 
    Definindo tela

--- Flappy.py
+++ Flappy.py
@@ -1,1 +1,11 @@
 #Começando o jogo
+import pygame
+from pygame.locals import *
+#definindo dimensões da tela do jogo
+LARGURA = 1920
+ALTURA = 1080
+#função que inicializa o pygame
+pygame.init()
+#criando a tela com as dimensões definidas
+tela=pygame.display.set_mode((LARGURA,ALTURA))
+
