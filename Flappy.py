b706dbb84d6e0ee73e7d11f7fde852b9cdae8b52
Lucas <lucasofa808@gmail.com>
2020-05-11 17:19:26 -0300

    Setup tela e fundo do jogo
    -Alterou-se o tamanho da tela
    -importou-se a imagem de fundo
    -deu-se um "rescale" na imagem do fundo para caber na nossa tela

--- Flappy.py
+++ Flappy.py
@@ -1,11 +1,34 @@
-#Começando o jogo
+# Importando bibliotecas
+
 import pygame
 from pygame.locals import *
-#definindo dimensões da tela do jogo
-LARGURA = 1920
-ALTURA = 1080
+
+#Definindo dimensões da tela do jogo
+
+LARGURA = 500
+ALTURA = 800
+
 #função que inicializa o pygame
 pygame.init()
-#criando a tela com as dimensões definidas
+# Criando a tela com as dimensões definidas
 tela=pygame.display.set_mode((LARGURA,ALTURA))
+#imporatando fundo e definindo uma "variável" para utilizá-lo
+FUNDO=pygame.image.load("fundo.png")
+#alterando tamanho da imagem do fundo para caber na tela
+FUNDO=pygame.transform.scale(FUNDO,(LARGURA,ALTURA))
 
+#loop principal do game 
+while True:
+    for evento in pygame.event.get():
+        # Se o usuário apertar no botão (x) de fechar, o jogo é fechado 
+        if evento.type == QUIT:
+            pygame.quit()
+    tela.blit(FUNDO,(0,0))
+    
+    
+    
+    
+    
+    
+    pygame.display.update()
+    
