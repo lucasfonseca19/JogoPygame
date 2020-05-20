e5622c6709876a5bbce6e79cc560e1e8e830b34e
maia <maiaf@al.insper.edu.br>
2020-05-20 16:41:23 -0300

    guria pula e cai corretamente

--- Flappy.py
+++ Flappy.py
@@ -7,7 +7,8 @@ from pygame.locals import *
 LARGURA = 400
 ALTURA = 800
 #constantes fisicas
-VELOCIDADE=2
+VELOCIDADE=2.5
+GRAVIDADE=0.08
 #criando classe do personagem (ela herda funções da classe sprite do pygame)
 class Guria(pygame.sprite.Sprite):
     def __init__(self):
@@ -16,17 +17,18 @@ class Guria(pygame.sprite.Sprite):
         #importando imagem do personagem e usando convert alpha para considerar apenas os pixel do icone(sem o fundo)
         self.image = pygame.image.load('guria.png').convert_alpha()
         self.image = pygame.transform.scale(self.image,(100,100))
+        self.velocidade = VELOCIDADE
         #posição e tamanho do personagem
         self.rect = self.image.get_rect()
         self.rect.center=(LARGURA/2,ALTURA/2)
-        self.velocidade= VELOCIDADE
-        
     #atualização do sprite com o tempo   
     def update(self):
+        self.velocidade += GRAVIDADE
         #update da altura
-        self.rect[1] +=VELOCIDADE
+        self.rect[1] += self.velocidade
+    #sprite pula
     def pular(self):
-        self.rect[1]-=8
+        self.velocidade =- VELOCIDADE
 #função que inicializa o pygame
 pygame.init()
 # Criando a tela com as dimensões definidas
@@ -43,25 +45,20 @@ guria=Guria()
 guria_grupo.add(guria)
 #loop principal do game 
 while True:
+   
     for evento in pygame.event.get():
         # Se o usuário apertar no botão (x) de fechar, o jogo é fechado 
         if evento.type == QUIT:
             pygame.quit()
+        if evento.type == pygame.KEYDOWN:
+            if evento.key==K_SPACE:
+                guria.pular()
     #colocando a imagem de fundo na tela na origem da dela (0,0)
-    tela.blit(FUNDO,(0,0))
+    tela.blit(FUNDO,(0,0))            
     #modificações do personagem
     guria_grupo.update()
     #colocando o personagem na tela
     guria_grupo.draw(tela)
-    if evento.type == pygame.KEYDOWN:
-        if evento.key==K_SPACE:
-            guria.pular()
-    
-
-    
-
-    
-    
     
     
     
